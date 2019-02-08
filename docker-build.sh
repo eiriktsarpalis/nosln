854a4cc23df2f114b6b5b9249f88cb612babc89e
Eirik Tsarpalis <eirik.tsarpalis@jet.com>
2019-02-08 15:10:32 +0000

    fix build issues

--- docker-build.sh
+++ docker-build.sh
@@ -8,7 +8,7 @@ CONTAINER_NAME=${3:-"docker-nosln-build-container"}
 docker build -t $IMAGE_LABEL .
 
 # dotnet build, test & nuget publish
-docker run --name $CONTAINER_NAME \
+docker run --name $CONTAINER_NAME --rm \
            -e NUGET_API_KEY=$NUGET_KEY \
            -e TARGETS=$TARGETS \
 		   $IMAGE_LABEL
